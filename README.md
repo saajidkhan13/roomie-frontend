614231287c0db67c08ab6d6a23edd11b46e94ab0
Rexman17 <a.stone1794@gmail.com>
2018-12-04 17:22:50 -0500

    added js script to index.html and got domcontentloaded event listener to work

---
+++ README.md
@@ -0,0 +1,6 @@
+## Our users should be able to...
+
+1. Log in
+2. check to see what apartment they belong to
+3. can see:
+  
---
+++ src/index.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('ur page loaded')
+}) // end DOMContentLoaded
