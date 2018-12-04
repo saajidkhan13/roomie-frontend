efb3f1f02214061402d5c7bd13c36d6729bf0dcc
Rexman17 <a.stone1794@gmail.com>
2018-12-04 17:39:13 -0500

    initial get working and helper

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('ur page loaded')
+  // DOM Elements
+  const apartmentsDiv = document.getElementById('apartment')
+  // Variables
+  const endPoint = 'http://localhost:3000/api/v1/apartments'
+
+  // Fetches and Event Listeners
+  fetch(endPoint)
+    .then(res => res.json())
+    .then(json => {
+      apartmentsDiv.innerHTML = showApartments(json)
+    });
+
+    // HELPERS
+    function showApartments(json) {
+      return json.map((apt) => {
+        return `<p>${apt.name}</p>
+                <button>Edit</button>`
+      }).join('')
+    }
+
+
+
+
+
+
+
+
 }) // end DOMContentLoaded
