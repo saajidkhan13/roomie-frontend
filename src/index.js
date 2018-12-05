53dff733fcaf3d33085086621250c50569e3c6f7
Saajid Khan <saajidkhan@Saajids-MacBook-Pro.local>
2018-12-05 14:35:37 -0500

    finally got roommates to show

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   // DOM Elements
   const apartmentDiv = document.getElementById("apartment-div")
   const showDiv = document.getElementById("show-div")
+  const aptStuff = document.getElementById('apt-stuff')
+  const roommates = document.getElementById('roommates-div')
+  console.log(roommates);
 
   // Variables
   const endPoint = 'http://localhost:3000/api/v1/apartments'
   let dataStore = []
+  let users= [];
+
 
   // Fetches and Event Listeners
   fetch(endPoint)
@@ -15,40 +20,47 @@ document.addEventListener('DOMContentLoaded', () => {
       dataStore = json
       // console.log(dataStore);
       apartmentDiv.innerHTML = showApartment(dataStore)
+      // roommates.innerHTML = x.join('')
+      users = dataStore[0].users
+      console.log(users);
+      roommates.innerHTML = showRoommates(users).join('')
+
     });
 
 
     // HELPERS
     function showApartment(dataStore) {
       return dataStore.map((apt) => {
-        return `<center><h1>${apt.name}</h1>
+        return `<div id="apt-stuff"><center><h1>${apt.name}</h1>
         <label>Address: </label>
-        <span>${apt.address}</span><br><br>
-        <button id='landlord-button'>Landlord Information</button>
-        <button id='necessities-button'>Things We Need</button>`
+        <span>${apt.address}</span><br><br></div>
+        <div id='roommates'></div>`
       }).join('')
+    }//end of showApartment
+
+    function showRoommates(users) {
+      return users.map((user) => {
+        return `<p>${user.name}</p>`
+      })
     }
 
 
+
+
     document.addEventListener('click', (event) => {
+      // debugger
       showDiv.innerHTML = ""
       if(event.target.id === 'landlord-button') {
+        debugger
         let landlordInfo =
         `<p>Our landlord is ${dataStore[0].landlord_name}</p>
         <p> Phone Number: ${dataStore[0].landlord_contact}</p>`
         showDiv.innerHTML = landlordInfo
       }//if event for landlord button
       else if(event.target.id === 'necessities-button'){
-        console.log('yerrrr');
-        let necArray = dataStore[0].necessities
-        debugger
-        let necJoin = necArray.join(", ")
-        console.log(necJoin);
-        // necArray.forEach((nec) => {
-        //   showDiv.innerHTML = `We need ${nec}`
-        // })
-        // let necInfo =
-        // `We need ${dataStore[0].necessities.split()}`
+        let necInfo =
+        `<p>We need to buy ${dataStore[0].necessities}</p>`
+        showDiv.innerHTML = necInfo
       }//end of else if
     })//end of click event listener
 
